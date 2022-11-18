0400190975d7b7c91edf513bc067a62fc9b818a5
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2022-11-18 02:12:20 +0200

    Create index.bash

---
+++ index.bash
@@ -0,0 +1,45 @@
+$ cd elections/
+$ touch candidates.txt
+$ touch issues.txt
+$ cd ../../../
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ cd daily-buzz
+bash: cd: daily-buzz: No such file or directory
+$ mkdir  business
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ cd business/
+$ pwd
+/home/ccuser/workspace/daily-buzz/business
+$ ls
+$ mkdir startups
+$ cd startups/
+$ pwd
+/home/ccuser/workspace/daily-buzz/business/startups
+$ cd ..
+$ pwd
+/home/ccuser/workspace/daily-buzz/business
+$ mkdir startups/disruptors
+$ touch tech.txt
+$ pwd
+/home/ccuser/workspace/daily-buzz/business
+$ cd disruptors
+bash: cd: disruptors: No such file or directory
+$ ls
+startups  tech.txt
+$ cd startups/
+$ ls
+disruptors
+$ cd disruptors/
+$ touch  tech.txt
+$ touch education.txt
+$ cd..
+bash: cd..: command not found
+$ cd ..
+$ pwd
+/home/ccuser/workspace/daily-buzz/business/startups
+$ cd ../..
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ 
